a44f98d63b27717f73d42a9a8ff56023c10ffb28
NoritakaIkeda <ikeda.noritaka271@mail.kyutech.jp>
2019-12-05 16:09:49 +0900

    20191205共有メモリのサンプルプログラムを作成

---
+++ 20191205recive.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+
+void main(){
+    int id;
+    char *adr;
+    if((id=shmget(IPC_PRIVATE,512,IPC_CREAT|0666))==-1){
+        perror("shmat");
+        exit(-1);
+    }
+    printf("共有メモリID=%d\n",id);
+    if((adr=shmat(id,0,0))==-1){
+        perror("shmat");
+    }else{
+        strcpy(adr,"Initial");
+        while(1){
+            printf("%s\n",adr);
+            if(strcmp(adr,"end")==0){
+                break;
+            }
+            sleep(3);
+        }
+        if(shmdt(adr)==-1){
+            perror("shmdt");
+        }
+    }
+    if(shmctl(id,IPC_RMID,0)==-1){
+        perror("shmctl");
+    }
+}
---
+++ 20191205send.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+
+void main(argc,argv)
+int argc;
+char *argv[];
+{
+int id;
+char *adr;
+
+if(argc<=1){
+fprintf(stderr,"共有メモリIDを引数に指定すること。\n");
+exit(-1);
+}
+
+id=atoi(argv[1]);
+
+if((adr=shmat(id,0,0))==-1){
+perror("shmat");
+}
+else{
+while(1){
+gets(adr);
+if(strcmp(adr,"end")==0){
+break;
+}
+}
+
+if(shmdt(adr)==-1){
+perror("shmdt");
+}
+}
+}
