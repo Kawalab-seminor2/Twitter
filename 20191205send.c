d4b56449b57c9d441a0102cc2a223ee0ee4b8795
NoritakaIkeda <ikeda.noritaka271@mail.kyutech.jp>
2019-12-12 15:45:41 +0900

    fixed20191212

--- 20191205send.c
+++ 20191205send.c
@@ -8,29 +8,47 @@ void main(argc,argv)
 int argc;
 char *argv[];
 {
-int id;
+int id,a;
 char *adr;
+int x=1;
+x=getpid();
 
-if(argc<=1){
-fprintf(stderr,"共有メモリIDを引数に指定すること。\n");
-exit(-1);
+scanf("%d", &a);
+switch (a) {
+	case 1:
+		if(argc<=1){
+            fprintf(stderr,"共有メモリIDを引数に指定すること。\n");
+            exit(-1);
+        }
+
+        id=atoi(argv[1]);
+
+        if((adr=shmat(id,0,0))==-1){
+            perror("shmat");
+        }else{
+            while(1){
+                printf("%d:",x);
+                gets(adr);
+                if(strcmp(adr,"end")==0){
+                    break;
+                }
+            }
+
+            if(shmdt(adr)==-1){
+                perror("shmdt");
+            }
+            }
+		break;
+	case 2:
+		printf("a = 3\n");
+		break;
+	case 5:
+		printf("a = 5\n");
+		break;
+	default:
+		printf("others\n");
+		break;
 }
 
-id=atoi(argv[1]);
 
-if((adr=shmat(id,0,0))==-1){
-perror("shmat");
-}
-else{
-while(1){
-gets(adr);
-if(strcmp(adr,"end")==0){
-break;
-}
-}
-
-if(shmdt(adr)==-1){
-perror("shmdt");
-}
-}
 }
