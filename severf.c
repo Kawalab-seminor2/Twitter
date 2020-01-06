840fbb061272bc77a534bd43195dac3a2847eaec
NoritakaIkeda <ikeda.noritaka271@mail.kyutech.jp>
2020-01-07 05:30:21 +0900

    bug fixed

---
+++ severf.c
@@ -0,0 +1,138 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+
+int main(){
+    int id;
+    char *adr,A[60],B[60],BB[60],C[60],D[60],E[60],EE[60],userA[140],userB[140];
+    char *ptr,Q[10];
+    
+    if((id=shmget(IPC_PRIVATE,512,IPC_CREAT|0666))==-1){
+        perror("shmat");
+        exit(-1);
+    }
+    printf("共有メモリID=%d\n",id);
+    if((adr=(char *)shmat(id,0,0))==(void *)-1){
+        perror("shmat");
+    }else{
+        printf("111共有メモリID=%d\n",id);
+        //strcpy(adr,"Initial");
+        //strcpy(A,"Initial");
+        strcpy(B,"a");
+        strcpy(BB,"aa");
+        strcpy(EE,"bb");
+        strcpy(E,"b");
+        strcpy(C,"userA");
+        strcpy(D,"userB");
+        printf("%s\n",adr);
+        while(1){ 
+            //strtok(adr,",");
+            printf("1 %s; \n",adr);
+            printf("222共有メモリID=%d\n",id);
+            //if(strcmp(A,adr)!=0){
+                printf("good0") ; 
+                if(strcmp(C,adr)==0){
+                    
+                    printf("good1") ; 
+                    printf("%s",userA);
+                    
+                        strcpy(adr,userA);
+                        sleep(3);
+                        shmdt(adr);
+                        if((adr=(char *)shmat(id,0,0))==(void *)-1){
+                            perror("shmat");
+                        }
+                        //*adr=userA[0];
+                        printf("%s\n",adr);
+                        //memset((void *) *adr, 0,256);
+                }else if(strcmp(D,adr)==0){
+                    
+                    printf("%s",userB);
+                    printf("good2") ;  
+                        strcpy(adr,userB);
+                        sleep(3);
+                        shmdt(adr);
+                        if((adr=(char *)shmat(id,0,0))==(void *)-1){
+                            perror("shmat");
+                        }
+                        //*adr=userA[0];
+                        printf("%s\n",adr);
+                        //memset((void *) *adr, 0,256);
+                }else{
+                printf("good3") ; 
+                strtok(adr,",");
+                printf("%s \n",adr);
+
+                if((strcmp(B,adr)==0)||(strcmp(BB,adr)==0)){
+                        adr=strtok(NULL,",");
+                        printf("userA=%s\n",adr);
+                        
+                        strcpy(userA,adr);
+                        
+                        sleep(1);
+                    shmdt(adr);
+if((adr=(char *)shmat(id,0,0))==(void *)-1){
+            perror("shmat");
+        }
+                        printf("%s\n",userA);
+                        
+                        *adr=Q[0];
+                        shmdt(adr);
+if((adr=(char *)shmat(id,0,0))==(void *)-1){
+            perror("shmat");
+        }
+                        //memset((void *) *adr, 0,256);
+                }else if((strcmp(E,adr)==0)||(strcmp(EE,adr)==0)){
+                        adr=strtok(NULL,",");
+                        printf("userB=%s\n",adr);
+                        
+                        strcpy(userB,adr);
+                        
+                        sleep(1);
+                    shmdt(adr);
+if((adr=(char *)shmat(id,0,0))==(void *)-1){
+            perror("shmat");
+        }
+                        printf("%s\n",userB);
+                        
+                        *adr=Q[0];
+                        shmdt(adr);
+if((adr=(char *)shmat(id,0,0))==(void *)-1){
+            perror("shmat");
+        }
+                        //memset((void *) *adr, 0,256);
+                }else{
+                    printf("判定ミス\n");
+                    
+                }  *adr=Q[0]; 
+                //memset((void *) *adr, 0,256);
+                }
+            //}else 
+            if(strcmp(adr,"end")==0){
+                            break;
+                }
+
+            
+
+            sleep(1);
+            
+            
+            //strcpy(A,adr);
+            }
+            if(shmdt(adr)==-1){
+                perror("shmdt");
+            }
+            
+    }
+        if(shmctl(id,IPC_RMID,0)==-1){
+            perror("shmctl");
+        }
+return 0;
+}
---
+++ usrA.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+int main(argc,argv)
+int argc;
+char *argv[];
+{
+int id,a;
+char *adr; 
+char A[60],b[140],c[140];
+char *x,*d,*e ;
+char *C,Q[10];
+int count=0;
+strcpy(c,"a");
+
+
+while(1){
+    printf("1:tweet 2:follow 9:exit -> ");
+    scanf("%d", &a);
+switch (a) {
+	case 1:
+		if(argc<=1){
+            fprintf(stderr,"共有メモリIDを引数に指定すること。\n");
+            exit(-1);
+        }
+
+        //id=atoi(argv[1]);
+
+        //if((adr=(char *)shmat(id,0,0))==(void *)-1){
+           // perror("shmat");
+        //}else{
+            while(1){
+                id=atoi(argv[1]);
+                if((adr=(char *)shmat(id,0,0))==(void *)-1){
+                    perror("shmat");
+                }
+                d="a";
+                printf("%s:",d);
+                scanf("%s", b);
+                strcat(adr,d);
+                if(count>=0){
+                    strcat(adr,d);
+                }
+                strcat(adr,","); printf("1");
+                strcat(adr,b);
+                printf("1");
+                //shmdt(adr);
+                count ++;
+                if(strcmp(adr,"end")==0){
+                    break;
+                }
+                break;
+            }
+
+            if(shmdt(adr)==-1){
+                perror("shmdt");
+            }
+            break;
+       // }
+		break;
+        case 2:
+            //if(strcmp(A,adr)!=0){
+            id=atoi(argv[1]);
+
+        if((adr=(char *)shmat(id,0,0))==(void *)-1){
+            perror("shmat");
+        }else{
+    
+        printf("1 %s\n",adr);
+        //*adr=Q[0]; 
+        printf("2 %s\n",adr);
+        e="userB"; 
+        strcpy(adr,e);
+            //if(strcmp(A,adr)!=0){
+        printf("3%s\n",adr);
+        sleep(1);
+        shmdt(adr);
+if((adr=(char *)shmat(id,0,0))==(void *)-1){
+            perror("shmat");
+        }
+
+        while(1){
+            if(strcmp(e,adr)!=0){
+                printf("%s\n",adr);
+                break;
+            }else if(strcmp(adr,"end")==0){
+                    break;
+                }
+            //strcpy(A,adr); 
+            sleep(3);
+            //if(strcmp(A,adr)!=0){
+
+        }
+        if(shmdt(adr)==-1){
+                perror("shmdt");
+            }
+
+        }	
+		break;
+	case 9:
+            //if(strcmp(A,adr)!=0){
+		exit(1);
+		break;
+	default:
+		printf("others\n");
+		break;
+}
+
+}
+
+return 0;
+}
